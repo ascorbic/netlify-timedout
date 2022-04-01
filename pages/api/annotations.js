fbbaebf078195d9cb6ac9612a96a6a788806a1b3
Max Kubik <50140834+mkubdev@users.noreply.github.com>
2022-04-01 13:57:25 +0200

    Update annotations.js

--- pages/api/annotations.js
+++ pages/api/annotations.js
@@ -1,39 +1,3 @@
-// const { connectToDatabase } = require('../../lib/mongodb');
-// const ObjectId = require('mongodb').ObjectId;
-
-// export default async function handler(req, res) {
-
-//   // switch the methods
-//   switch (req.method) {
-//       case 'GET': {
-//           return getAllAnnotations(req, res);
-//       }
-//       // case 'PUT': { ... }
-//   }
-// }
-
-// async function getAllAnnotations(req,res){
-//   try {
-//       // connect to the database
-//       const { db } = await connectToDatabase();
-
-//       // fetch the annotations
-//       let annotations = await db
-//           .collection('annotations')
-//           .find({})
-//           .sort({ id: 1 })
-//           .toArray();
-
-//       // return the annotations
-//       return res.json({
-//         annotations: JSON.parse(JSON.stringify(annotations)),
-//           success: true,
-//       });
-//   } catch (error) {
-//       console.error(error)
-//   }
-// }
-
 import prisma from '../../lib/prisma'
 
 
