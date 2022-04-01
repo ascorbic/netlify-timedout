ffd9a3ecf8e4e9c3de4dde7bdac5d67038b53017
Matt Kane <m@mk.gg>
2022-04-01 18:59:30 +0100

    uncomment

--- pages/api/annotations.js
+++ pages/api/annotations.js
@@ -1,16 +1,17 @@
-// import prisma from '../../lib/prisma'
+import prisma from '../../lib/prisma'
 
 
 export default async function handle(req, res) {
   try {
     console.log("about to get annotations")
-    // console.log(prisma)
-    res.json({hello: 1})
+    console.log(prisma)
+    res.status(200).json({hello: 1})
     // const annotations = await prisma.annotations.findMany()
     // console.log("got annotations")
     // res.json(annotations)
   } catch (error) {
     console.error(error)
+    res.status(500).json({ error })
   }
 }
 
