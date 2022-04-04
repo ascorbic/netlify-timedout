00a519293c5c36283a598c0ce5f003d888d7a09d
Matt Kane <m@mk.gg>
2022-04-04 06:28:34 +0100

    add local next

--- pages/api/annotations.js
+++ pages/api/annotations.js
@@ -4,7 +4,6 @@ console.log("imported annotations module", new Date().toISOString())
 export default async function handle(req, res) {
   try {
     console.log("about to get annotations")
-    console.log(prisma)
     const annotations = await prisma.annotations.findMany()
     console.log("got annotations")
     res.json(annotations)
